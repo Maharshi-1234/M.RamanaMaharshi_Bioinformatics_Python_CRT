4013922460867e557ec6fa006e23f782f0c2d76e
M.Ramana Maharshi <mellacheruvuramanamaharshi@gmail.com>
2025-06-18 18:43:18 +0530

    Delete OOPS directory

--- OOPS/Demo.py
+++
@@ -1,5 +0,0 @@
-class Student:
-    def __init__(self):
-        pass
-S1=Student()
-print(type(S1))
--- OOPS/Demo2.py
+++
@@ -1,6 +0,0 @@
-class Employee:
-    def __init__(self):
-        print("Employee class constructor has been called")
-E1=Employee()
-E2=Employee()
-E3=Employee()
