15bd3e08e9b02a01d4a3234bbc2312a9a28c3e7d
M.Ramana Maharshi <mellacheruvuramanamaharshi@gmail.com>
2025-06-24 18:40:08 +0530

    Add files via upload

---
+++ DSA/COMPREHENSIONS/List.py
@@ -0,0 +1,9 @@
+#Creation of new list from an iterable object that satisy given condition.
+'''Syntax: 
+new_list = [expression for the item in iterable_object if_statement]
+There can be zero or more if statements 
+There can be one or more for loops. 
+Ex: list1 = [i+1 for i in range(20)]'''
+
+nums = [i for i in range(1,11)]
+print(nums)
---
+++ DSA/COMPREHENSIONS/Ques.py
@@ -0,0 +1,1 @@
+#Write a python program to print even numbers from 1 to N uing list comprehension
